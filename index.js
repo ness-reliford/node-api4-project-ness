505df15b754ac8b39991483207c76c348f554387
Ness Reliford <nessreliford@gmail.com>
2024-12-10 17:36:29 -0800

    port in use error - stuck

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 require('dotenv').config()
 
-console.log(process.env.PORT, process.env.NODE_ENV)
+const express = require('express')
+const cors = require('cors')
+
+const server = express()
+
+const PORT = process.env.PORT || 9000
+
+server.use(express.json())
+server.use(cors())
+
+server.get('/api/hello', (req, res, next) => {
+    res.json({
+        message: 'api is working'
+    })
+})
+
+server.use('*', (req, res, next) => {
+    res.send(`<h1>Hello, there!</h1>`)
+})
+
+server.use((err, req, res, next) =>{
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack,
+    })
+})
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+})
